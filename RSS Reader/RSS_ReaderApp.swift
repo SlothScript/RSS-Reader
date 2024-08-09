9f33cc550c9ed97e5dbd8c03fc782c0abeb3d6dc
SlothScript <106759481+SlothScript@users.noreply.github.com>
2024-08-09 16:46:44 -0500

    Initial Commit

---
+++ RSS Reader/RSS_ReaderApp.swift
@@ -0,0 +1,32 @@
+//
+//  RSS_ReaderApp.swift
+//  RSS Reader
+//
+//  Created by Nathaniel Cole on 8/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RSS_ReaderApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
